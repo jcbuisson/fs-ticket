d7d4e87bfde5be16fb27566eb108328bf1c76c4d
JC BUISSON <buisson@enseeiht.fr>
2024-01-10 08:01:58 +0100

    WIP

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,20 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({ command, mode }) => {
+   return {
+      plugins: [
+      ],
+      server: {
+         port: 8000,
+         open: true,
+         proxy: {
+            '^/api/.*': {
+               target: 'http://localhost:3000',
+               ws: true,
+               secure: false,
+               changeOrigin: true,
+            },
+         }
+      },
+   }
 })
