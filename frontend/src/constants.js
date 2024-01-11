fe099e68c9c45a2012cdf4a41c0fd62c8453664c
JC BUISSON <buisson@enseeiht.fr>
2024-01-11 10:51:13 +0100

    WIP

---
+++ frontend/src/constants.js
@@ -0,0 +1,12 @@
+
+export const CATEGORIES = {
+   furniture: "Mobilier",
+   computer: "Informatique",
+   other: "Autre",
+}
+
+export const PRIORITIES = {
+   low: "Basse",
+   normal: "Normale",
+   high: "Haute",
+}
