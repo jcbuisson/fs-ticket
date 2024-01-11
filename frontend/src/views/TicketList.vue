0acdb33f19919773df400a48a2c7e2d58f3fc2a7
JC BUISSON <buisson@enseeiht.fr>
2024-01-11 09:30:22 +0100

    WIP

--- frontend/src/views/TicketList.vue
+++ frontend/src/views/TicketList.vue
@@ -10,9 +10,12 @@
                      {{ ticket.description }}
                   </p>
                </div>
-               <div class="px-6 pt-4 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ ticket.category }}</span>
-                  <span class="inline-block bg-red-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ ticket.priority }}</span>
+               <div class="px-6 pt-2 pb-2">
+                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ CATEGORIES[ticket.category] }}</span>
+                  <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                     :class="{'bg-red-200': ticket.priority === 'high', 'bg-orange-200': ticket.priority === 'normal', 'bg-yellow-200': ticket.priority === 'low',  }">
+                     {{ PRIORITIES[ticket.priority] }}
+                  </span>
                </div>
             </div>
          </template>
@@ -21,7 +24,7 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue'
+import { ref, onMounted, computed } from 'vue'
 
 const ticketList = ref([])
 
@@ -34,4 +37,17 @@ onMounted(async () => {
    })
    ticketList.value = await response.json()
 })
+
+const CATEGORIES = {
+   furniture: "Mobilier",
+   computer: "Informatique",
+   other: "Autre",
+}
+
+const PRIORITIES = {
+   low: "Basse",
+   normal: "Normale",
+   high: "Haute",
+}
+
 </script>
