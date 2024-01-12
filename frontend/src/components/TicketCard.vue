39fb3805d8d6109ea9653b7df9b0d87dca9d019b
JC BUISSON <buisson@enseeiht.fr>
2024-01-12 14:02:42 +0100

    WIP

--- frontend/src/components/TicketCard.vue
+++ frontend/src/components/TicketCard.vue
@@ -2,7 +2,8 @@
    <div class="w-80 rounded shadow-lg cursor-pointer" :class="{'bg-gray-200': selected}">
       <div class="px-6 py-4">
          <div class="font-bold text-xl mb-2">Ticket #{{ ticket.id }}</div>
-         <div class="text-lg mb-2">Créé le {{ format(new Date(ticket.created_at || '1970-01-01'), 'dd/MM/yyyy HH:mm') }}</div>
+         <div class="mb-2">Créé le {{ formatDate(ticket.created_at) }}</div>
+         <div class="mb-2">Créé le {{ formatDate(ticket.created_at) }}</div>
          <p class="text-gray-700 text-sm">
             {{ ticket.description }}
          </p>
@@ -42,4 +43,9 @@ watch(() => props.ticketId, async () => {
 }, {
    immediate: true
 })
+
+function formatDate(isoDate) {
+   if (!isoDate) return ''
+   return format(new Date(isoDate), 'dd/MM/yyyy HH:mm')
+}
 </script>
