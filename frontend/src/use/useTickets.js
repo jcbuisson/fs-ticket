c4d028f8ab517ea29835975ebec3da9023ce53f9
JC BUISSON <buisson@enseeiht.fr>
2024-01-12 06:33:09 +0100

    WIP

--- frontend/src/use/useTickets.js
+++ frontend/src/use/useTickets.js
@@ -1,21 +1,27 @@
+import { useSessionStorage } from '@vueuse/core'
 
-const id2ticket = {}
+const id2ticket = useSessionStorage('id2ticket', {})
+const ticketListComplete = useSessionStorage('ticket-list-complete', false)
 
 
 export async function getAllTickets() {
+   if (ticketListComplete.value) {
+      return Object.values(id2ticket.value)
+   }
    const response = await fetch('/api/ticket')
    const ticketList = await response.json()
    for (const ticket of ticketList) {
-      id2ticket[ticket.id] = ticket
+      id2ticket.value[ticket.id] = ticket
    }
+   ticketListComplete.value = true
    return ticketList
 }
 
 export async function asyncTicket(ticketId) {
-   if (id2ticket[ticketId] === undefined) {
+   if (id2ticket.value[ticketId] === undefined) {
       const response = await fetch(`/api/ticket/${ticketId}`)
       const ticket = await response.json()
-      id2ticket[ticket.id] = ticket
+      id2ticket.value[ticket.id] = ticket
    }
-   return id2ticket[ticketId]
+   return id2ticket.value[ticketId]
 }
