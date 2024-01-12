1fbf440b60d33673c5ac9c3772e084ecc9fbd7b3
JC BUISSON <buisson@enseeiht.fr>
2024-01-12 10:01:17 +0100

    WIP

--- frontend/src/views/TicketList.vue
+++ frontend/src/views/TicketList.vue
@@ -1,25 +1,22 @@
 <template>
    <div class="max-w-xl mx-auto mt-8">
-
-      <!-- <div class="grid grid-cols-2 gap-3"> -->
-         <template v-for="ticket in ticketList" class="p-4">
-            <div class="max-w-sm rounded overflow-hidden shadow-lg">
-               <div class="px-6 py-4">
-                  <div class="font-bold text-xl mb-2">Ticket #{{ ticket.id }}</div>
-                  <p class="text-gray-700 text-sm">
-                     {{ ticket.description }}
-                  </p>
-               </div>
-               <div class="px-6 pt-2 pb-2">
-                  <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ CATEGORIES[ticket.category] }}</span>
-                  <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                     :class="{'bg-red-200': ticket.priority === 'high', 'bg-orange-200': ticket.priority === 'normal', 'bg-yellow-200': ticket.priority === 'low',  }">
-                     {{ PRIORITIES[ticket.priority] }}
-                  </span>
-               </div>
+      <template v-for="ticket in ticketList">
+         <div class="max-w-sm rounded overflow-hidden shadow-lg">
+            <div class="px-6 py-4">
+               <div class="font-bold text-xl mb-2">Ticket #{{ ticket.id }}</div>
+               <p class="text-gray-700 text-sm">
+                  {{ ticket.description }}
+               </p>
             </div>
-         </template>
-      <!-- </div> -->
+            <div class="px-6 pt-2 pb-2">
+               <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{{ CATEGORIES[ticket.category] }}</span>
+               <span class="inline-block rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+                  :class="{'bg-red-200': ticket.priority === 'high', 'bg-orange-200': ticket.priority === 'normal', 'bg-yellow-200': ticket.priority === 'low',  }">
+                  {{ PRIORITIES[ticket.priority] }}
+               </span>
+            </div>
+         </div>
+      </template>
    </div>
 </template>
 
