3b527ce4334e0d46a698d73bae322c3f219f9a5f
JC BUISSON <buisson@enseeiht.fr>
2024-01-18 09:48:30 +0100

    WIP

--- frontend/src/use/useTickets.js
+++ frontend/src/use/useTickets.js
@@ -7,26 +7,24 @@ const ticketListComplete = ref(false)
 
 
 export const allTickets = computed(() => {
-   if (ticketListComplete.value) {
-      return Object.values(id2ticket.value)
-   }
+   if (ticketListComplete.value) return Object.values(id2ticket.value)
    fetch('/api/ticket').then(response => response.json()).then(ticketList => {
       for (const ticket of ticketList) {
          id2ticket.value[ticket.id] = ticket
       }
       ticketListComplete.value = true
    })
-   return {}
+   return []
 })
 
 
 export async function asyncTicket(ticketId) {
-   if (id2ticket.value[ticketId] === undefined) {
-      const response = await fetch(`/api/ticket/${ticketId}`)
-      const ticket = await response.json()
-      id2ticket.value[ticket.id] = ticket
-   }
-   return id2ticket.value[ticketId]
+   let ticket = id2ticket.value[ticketId]
+   if (ticket) return ticket
+   const response = await fetch(`/api/ticket/${ticketId}`)
+   ticket = await response.json()
+   id2ticket.value[ticket.id] = ticket
+   return ticket
 }
 
 export const ticketOfId = computed(() => (id) => {
