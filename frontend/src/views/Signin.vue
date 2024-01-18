279250a84acefa0d16876c73b634fe7f8cbb01b6
JC BUISSON <buisson@enseeiht.fr>
2024-01-18 12:05:35 +0100

    WIP

--- frontend/src/views/Signin.vue
+++ frontend/src/views/Signin.vue
@@ -1,17 +1,28 @@
 <template>
-   <div class="w-full max-w-xs">
+   <div class="max-w-md mx-auto mt-8">
       <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
          <div class="mb-4">
-            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+            <label class="block text-gray-700 text-sm font-bold mb-2">
                Email
             </label>
-            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="Username">
+            <input type="email" v-model="email" class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" placeholder="Username">
          </div>
          <div class="flex items-center justify-between">
-            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+            <button @click="signin" :disabled="!email" class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline">
                Sign In
             </button>
          </div>
       </form>
    </div>
 </template>
+
+<script setup>
+import { ref } from 'vue'
+import router from '/src/router'
+
+const email = ref()
+
+function signin() {
+   
+}
+</script>
