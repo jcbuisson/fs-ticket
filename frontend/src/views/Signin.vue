39d3512d36f59c5d2e2aec3a784a3073df461f35
JC BUISSON <buisson@enseeiht.fr>
2024-01-18 07:39:40 +0100

    WIP

---
+++ frontend/src/views/Signin.vue
@@ -0,0 +1,17 @@
+<template>
+   <div class="w-full max-w-xs">
+      <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+         <div class="mb-4">
+            <label class="block text-gray-700 text-sm font-bold mb-2" for="username">
+               Email
+            </label>
+            <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" type="email" placeholder="Username">
+         </div>
+         <div class="flex items-center justify-between">
+            <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
+               Sign In
+            </button>
+         </div>
+      </form>
+   </div>
+</template>
